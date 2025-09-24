ca4ee92bb87ef8d66876ee75d066dbc1033467b0
Nicolas <nicolasmailhot92@gmail.com>
2025-09-23 22:24:25 -0400

    New package added, clean start.sh

--- start.sh
+++ start.sh
@@ -6,17 +6,11 @@ sudo pacman Syu
 #install Yay
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 
-#install Ghostty
-yay -Sy --noconfirm ghostty-git
-
 #install Nerd Font
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/Cousine.zip
 mkdir /usr/local/share/fonts/c
 bsdtar xvf Cousine.zip -C /usr/local/share/fonts/c
 rm Cousine.zip
-
-#install neovim
-yay -Sy --noconfirm neovim
 
 #install stow
 yay -Sy --noconfirm stow
